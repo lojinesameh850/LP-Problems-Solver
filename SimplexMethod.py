42527a74fa5b3af6f9aa4c579c3924a2a2bede8e
NourrKhalled <nourrkhalled123@gmail.com>
2025-03-20 03:11:15 +0200

    Update SimplexMethod.py

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -27,7 +27,7 @@ def handleUrVariables(A, c, urVariables):
 
 def simplex(c, A, b, optimalityCondition, urVariables=[]):
 
-    c = np.array(c)
+    c = np.array(c) 
     A = np.array(A)
     b = np.array(b)
 
@@ -53,42 +53,8 @@ def simplex(c, A, b, optimalityCondition, urVariables=[]):
     else:
         tableau = np.vstack([tableau, -1 * c])
 
-    print("Initial Tableau:")
-    print(tableau)
-
-    # Simplex algorithm
-    while True:
-        # Check for optimality
-        if all(tableau[-1, :-1] >= -TOL):
-            break
-
-        # Find pivot column
-        pivotCol = np.argmin(tableau[-1, :-1])
-
-        
-        if all(tableau[:-1, pivotCol] <= TOL):
-            # print("Problem is unbounded")
-            exit()
-
-        # Find pivot row
-        ratios = tableau[:-1, -1] / tableau[:-1, pivotCol]
-        ratios[ratios <= TOL] = np.inf 
-        pivotRow = np.argmin(ratios)
-
-        print("Pivot Column:", pivotCol)
-        print("Pivot Row:", pivotRow)
-        print("Pivot Element:", tableau[pivotRow, pivotCol])
-
-        # Pivot operation
-        pivot = tableau[pivotRow, pivotCol]
-        tableau[pivotRow, :] /= pivot
-        for i in range(constraints + 1):
-            if i != pivotRow:
-                tableau[i, :] -= tableau[i, pivotCol] * tableau[pivotRow, :]
-
-        print("Updated Tableau:")
-        print(tableau)
-
+    # print("Initial Tableau:")
+    tableau = solveTableau(tableau, constraints)
     # Solution
     solution = np.zeros(numOfVars)
     for i in range(numOfVars):
@@ -109,12 +75,51 @@ def simplex(c, A, b, optimalityCondition, urVariables=[]):
     
     return solution, objectiveValue
 
-# # Test Case
-# c = [-1, -1]
-# A = [[2, 3], [-3, 2], [3, -5]]
-# b = [12, -4, 2]
 
-# urVariables = [0]
+def solveTableau(tableau, constraints):
+    # Simplex algorithm
+    # print(tableau)
+    while True:
+        # Check for optimality
+        if all(tableau[-1, :-1] >= -TOL):
+            break
+
+        # Find pivot column
+        pivotCol = np.argmin(tableau[-1, :-1])
+
+        
+        if all(tableau[:-1, pivotCol] <= TOL):
+            # print("Problem is unbounded")
+            return None
+
+        # Find pivot row
+        ratios = tableau[:-1, -1] / tableau[:-1, pivotCol]
+        ratios[ratios <= TOL] = np.inf 
+        pivotRow = np.argmin(ratios)
+
+        # print("Pivot Column:", pivotCol)
+        # print("Pivot Row:", pivotRow)
+        # print("Pivot Element:", tableau[pivotRow, pivotCol])
+
+        # Pivot operation
+        pivot = tableau[pivotRow, pivotCol]
+        tableau[pivotRow, :] /= pivot
+        for i in range(constraints + 1):
+            if i != pivotRow:
+                tableau[i, :] -= tableau[i, pivotCol] * tableau[pivotRow, :]
+
+        # print("Updated Tableau:")
+        # print(tableau)
+    return tableau
+
+    
+
+# Test Case
+# c = [5, -4, 6, -8]
+# A = [[1, 2, 2, 4], [2, -1, 1, 2], [4, -2, 1, -1]]
+# b = [40, 8, 10]
+
+# urVariables = []
 
 # solution, objectiveValue = simplex(c, A, b, "minimize", urVariables)
 # print("Solution:", solution)
